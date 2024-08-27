3d0dcef4daabba590bfbb200d30a39c10ecce505
Ali-E-Oun <aliessamoun@gmail.com>
2024-08-27 14:07:33 +1000

    Bunch of Gui changes done by yours truly Ali Oun

--- Remote Control/app.py
+++ Remote Control/app.py
@@ -1,91 +1,82 @@
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout 
-from kivy.uix.label import Label 
-from kivy.uix.image import Image 
-from kivy.uix.button import Button 
-from kivy.uix.textinput import TextInput
-from kivy.clock import mainthread, Clock
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label  # Import Label
+from kivy.clock import Clock
 from kivy.uix.widget import Widget
+from kivy.core.window import Window
 import socket
-import time
 import Action
-import threading
 
-robot_ip = "127.0.0.1"
-robot_port = 12342
-send_cond = True 
+Window.clearcolor = (1, 1, 1, 1)  # changes the colour of the background to white 
 
+robot_ip = "192.168.200.243"
+robot_port = 50514
 robot_id = 1
 
-def send(action:Action):
+def send(action: Action):
     remote_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    msg:bytes = action.encode()
-    remote_socket.sendto(msg,(robot_ip, robot_port))
+    msg: bytes = action.encode()
+    remote_socket.sendto(msg, (robot_ip, robot_port))
     print(f'sending Action {action}')
 
+class MyRemote(FloatLayout):
 
-def listen():
-    pass
-
-
-class MyRemote(Widget):
-    
-
-    def StartForward(self):
-        Clock.schedule_interval(self.movingForward, 0.01)
+    def __init__(self, **kwargs):
+        super(MyRemote, self).__init__(**kwargs)
         
-    def StopForward(self):
+        self.label = Label(
+            text="Remote Control",  
+            size_hint=(None, None),  
+            pos_hint={'center_x': 0.5, 'top': 1})  
+        
+        self.add_widget(self.label)
+
+    def StartForward(self, instance):
+        Clock.schedule_interval(self.movingForward, 0.05)
+
+    def StopForward(self, instance):
         Clock.unschedule(self.movingForward)
 
-    def StartBackward(self):
-        Clock.schedule_interval(self.movingBackward, 0.01)
+    def StartBackward(self, instance):
+        Clock.schedule_interval(self.movingBackward, 0.05)
 
-    def StopBackward(self):
+    def StopBackward(self, instance):
         Clock.unschedule(self.movingBackward)
-    
-    def StartLeft(self):
-        Clock.schedule_interval(self.movingLeft, 0.01)
 
-    def StopLeft(self):
+    def StartLeft(self, instance):
+        Clock.schedule_interval(self.movingLeft, 0.05)
+
+    def StopLeft(self, instance):
         Clock.unschedule(self.movingLeft)
 
-    def StartRight(self):
-        Clock.schedule_interval(self.movingRight, 0.01)
+    def StartRight(self, instance):
+        Clock.schedule_interval(self.movingRight, 0.05)
 
-    def StopRight(self):
+    def StopRight(self, instance):
         Clock.unschedule(self.movingRight)
 
     def movingForward(self, dt):
-        action = Action.Action(robot_id=robot_id, vy = 5)
+        action = Action.Action(robot_id=robot_id, vy=5)
         send(action)
-        # print(f'{action}')
-    
+
     def movingBackward(self, dt):
-        action = Action.Action(robot_id=robot_id , vy = -5)
+        action = Action.Action(robot_id=robot_id, vy=-5)
         send(action)
-        # print(f'{action}')
-    
+
     def movingLeft(self, dt):
-        action = Action.Action(robot_id=robot_id , vx = -5)
+        action = Action.Action(robot_id=robot_id, vx=-5)
         send(action)
-        # print(f'{action}')
-    
+
     def movingRight(self, dt):
-        action = Action.Action(robot_id=robot_id, vx = 5)
+        action = Action.Action(robot_id=robot_id, vx=5)
         send(action)
-        # print(f'{action}')
-
-    
-
 
 
 class MyApp(App):
     def build(self):
         return MyRemote()
-    
+
+
 if __name__ == "__main__":
-   MyApp().run()
-
-
-    
-
+    MyApp().run()
--- Remote Control/demo_receiver.py
+++ Remote Control/demo_receiver.py
@@ -4,7 +4,7 @@ from Action import Action
 
 def main(): 
     recv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    recv_sock.bind(("127.0.0.1", 12342))
+    recv_sock.bind(("192.168.200.243", 50514))
     while True:
         data,_ = recv_sock.recvfrom(2048)
         action = Action.decode(data)
