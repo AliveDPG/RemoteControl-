de793ac04d07aaa174257872ad580b85051f5c6f
Emma Tsang <20544166@student.westernsydney.edu.au>
2024-08-27 16:31:35 +1000

    added textbox, different buttons

--- Remote Control/app.py
+++ Remote Control/app.py
@@ -7,7 +7,7 @@ from kivy.clock import Clock
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
 import socket
-import Action
+from Action import Action
 
 class IpId(BoxLayout):
     pass
@@ -17,6 +17,8 @@ Window.clearcolor = (1, 1, 1, 1)  # changes the colour of the background to whit
 robot_ip = "192.168.200.243"
 robot_port = 50514
 robot_id = 1
+DRIBBLE = False
+INTERVAL = 0.05 # second
 
 def send(action: Action):
     remote_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -26,45 +28,90 @@ def send(action: Action):
 
 class MyRemote(FloatLayout):
     def StartForward(self, instance):
-        Clock.schedule_interval(self.movingForward, 0.05)
+        Clock.schedule_interval(self.movingForward, INTERVAL)
 
     def StopForward(self, instance):
         Clock.unschedule(self.movingForward)
 
     def StartBackward(self, instance):
-        Clock.schedule_interval(self.movingBackward, 0.05)
+        Clock.schedule_interval(self.movingBackward, INTERVAL)
 
     def StopBackward(self, instance):
         Clock.unschedule(self.movingBackward)
 
     def StartLeft(self, instance):
-        Clock.schedule_interval(self.movingLeft, 0.05)
+        Clock.schedule_interval(self.movingLeft, INTERVAL)
 
     def StopLeft(self, instance):
         Clock.unschedule(self.movingLeft)
 
     def StartRight(self, instance):
-        Clock.schedule_interval(self.movingRight, 0.05)
+        Clock.schedule_interval(self.movingRight, INTERVAL)
 
     def StopRight(self, instance):
         Clock.unschedule(self.movingRight)
+    
+    def StartTurningRight(self,instance):
+        Clock.schedule_interval(self.turnRight,INTERVAL)
+    
+    def StartTurningLeft(self,instance):
+        Clock.schedule_interval(self.turnLeft,INTERVAL)
+    
+    def StopTurningRight(self,instance):
+        Clock.unschedule(self.turnRight)
+        
+    def StopTurningLeft(self,instance):
+        Clock.unschedule(self.turnLeft)
+
+    def StartKick(self,instance):
+        Clock.schedule_interval(self.kick,INTERVAL)
+
+    def ToggleDribble(self,instance):
+        Clock.schedule_interval(self.dribble,INTERVAL)
+    
 
     def movingForward(self, dt):
-        action = Action.Action(robot_id=robot_id, vy=5)
+        action = Action(robot_id=robot_id, vy=5)
         send(action)
 
     def movingBackward(self, dt):
-        action = Action.Action(robot_id=robot_id, vy=-5)
+        action = Action(robot_id=robot_id, vy=-5)
         send(action)
 
     def movingLeft(self, dt):
-        action = Action.Action(robot_id=robot_id, vx=-5)
+        action = Action(robot_id=robot_id, vx=-5)
         send(action)
 
     def movingRight(self, dt):
-        action = Action.Action(robot_id=robot_id, vx=5)
+        action = Action(robot_id=robot_id, vx=5)
+        send(action)
+    
+    def turnRight(self,dt):
+        action = Action(robot_id,w=0.5)
+        send(action)
+        
+    def turnLeft(self,dt):
+        action = Action(robot_id,w=-0.5)
+        send(action)
+    
+    def kick(self, dt):
+        action = Action(robot_id,k=1)
+        send(action)
+        
+    def dribble(self,dt):
+        d = toggle()
+        if d is True:
+            action = Action(robot_id, d=1)
+        else:
+            action = Action(robot_id,d=0)
         send(action)
 
+def toggle():
+    if DRIBBLE is False:
+        DRIBBLE = True
+    elif DRIBBLE is True:
+        DRIBBLE = False
+    return DRIBBLE
 
 class MyApp(App):
     def build(self):
